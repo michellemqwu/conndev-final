fb08bfcba1b874dad3dd35eff4733ed44494f43f
Michelle Wu <michellewu@a-10-27-17-91.dynapool.vpn.nyu.edu>
2023-05-18 13:10:34 +0800

    designed numbers

--- flask-api-server/app.py
+++ flask-api-server/app.py
@@ -13,6 +13,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="a3bf88ded7
                                                            client_secret="fe01a1b08a214b769d8c7893e9a21da9"))
 
 target_artist_id = '6RHTUrRF63xao58xh9FXYJ'
+target_artist_name = 'IVE'
 target_subreddit = 'IVE'
 
 app = Flask(__name__)
@@ -21,19 +22,19 @@ app = Flask(__name__)
 def get_melon_data():
     melon_chart = sp.playlist("6kbzPEHj3uMPRFsR3v6xzE")
     results = extract_tracks_from_playlist(melon_chart)
-    return jsonify(results)
+    return jsonify(results), {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/topfifty', methods=['GET'])
 def get_top_fifty_data():
     top_fifty_south_korea = sp.playlist('37i9dQZEVXbNxXF4SkHj9F')
     results = extract_tracks_from_playlist(top_fifty_south_korea)
-    return jsonify(results)
+    return jsonify(results), {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/kclubparty', methods=['GET'])
 def get_kclub_party_data():
     k_club_party = sp.playlist('37i9dQZF1DX4RDXswvP6Mj')
     results = extract_tracks_from_playlist(k_club_party)
-    return jsonify(results)
+    return jsonify(results), {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/reddithot', methods=['GET'])
 def get_reddit_posts():
@@ -44,7 +45,7 @@ def get_reddit_posts():
             utc = int(submission.created_utc)
             timestamp = datetime.utcfromtimestamp(utc).strftime('%Y-%m-%d %H:%M:%S')
             results.append({'timestamp' : timestamp, 'title' : submission.title, 'context' : submission.selftext})
-    return jsonify({target_subreddit : results})
+    return jsonify({target_subreddit : results}), {"Access-Control-Allow-Origin": "*"}
 
 def extract_tracks_from_playlist(playlist):
     results = []
@@ -55,5 +56,5 @@ def extract_tracks_from_playlist(playlist):
                 track_name = item['track']['name']
                 popularity = item['track']['popularity']
                 ranking = idx + 1
-                results.append({track_name : {'popularity' : popularity, 'ranking' : ranking}})
-    return {target_artist_id : results}
+                results.append({'track' : track_name, 'popularity' : popularity, 'ranking' : ranking})
+    return {target_artist_name : results}
